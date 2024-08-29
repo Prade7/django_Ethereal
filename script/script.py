ad11ee04af01d5b186ff7e1b6a46703d7c9b58ed
prade7 <pradeep112.pk7@gmail.com>
2024-08-29 05:52:57 +0530

    changed script file

--- script/script.py
+++ script/script.py
@@ -113,7 +113,7 @@ def main():
         machines = parse_cnc_file(file_path)
         for machine in machines:
             send_machine_data(machine)
-        time.sleep(1)
+        time.sleep(0.5)
 
 if __name__ == '__main__':
     main()
